1fdc0fc46bf6d5bad7f114238d8c6d92b314d513
xLeeVeNx <berdyev.damil1@mail.ru>
2021-04-24 23:27:50 +0600

    Урок №19

--- js/main.js
+++ js/main.js
@@ -6,6 +6,60 @@ window.addEventListener('DOMContentLoaded', function () {
     else return number;
   };
 
+  const popupOpenAnimation = () => {
+    const popup = document.querySelector('.popup-content');
+    let opacity = 0;
+    let opacityBg = 0;
+    let animationId;
+
+    popup.parentNode.style.backgroundColor = `rgba(0,0,0,0)`;
+    popup.style.opacity = opacity;
+
+    // Element's animation
+    const animate = () => {
+      if (opacity <= 1) {
+        opacity += 0.03;
+        opacityBg += 0.015;
+
+        popup.style.opacity = opacity;
+        popup.parentNode.style.backgroundColor = `rgba(0,0,0,${opacityBg})`;
+
+        animationId = requestAnimationFrame(animate);
+      } else {
+        cancelAnimationFrame(animationId);
+        popup.style.opacity = 1;
+        popup.parentNode.style.backgroundColor = `rgba(0,0,0,0.5})`;
+      }
+    };
+
+    animationId = requestAnimationFrame(animate);
+  };
+
+  const popupCloseAnimation = () => {
+    const popup = document.querySelector('.popup-content');
+    let opacity = 1;
+    let opacityBg = 0.5;
+    let animationId;
+
+    // Element's animation
+    const animate = () => {
+      if (opacity >= 0) {
+        opacity -= 0.03;
+        opacityBg -= 0.015;
+
+        popup.style.opacity = opacity;
+        popup.parentNode.style.backgroundColor = `rgba(0,0,0,${opacityBg})`;
+
+        animationId = requestAnimationFrame(animate);
+      } else {
+        cancelAnimationFrame(animationId);
+        popup.parentNode.style.display = 'none';
+      }
+    };
+
+    animationId = requestAnimationFrame(animate);
+  };
+
   // Timer
   const timer = (deadline) => {
     // DOM-elements
@@ -49,7 +103,50 @@ window.addEventListener('DOMContentLoaded', function () {
     idUpdateTimer = setInterval(updateTimer, 1000);
   };
 
-  // Timer's end
+  // Menu
+  const toggleMenu = () => {
+    const menuBtn   = document.querySelector('.menu');
+    const menuList  = document.querySelector('menu');
+    const closeBtn  = document.querySelector('.close-btn');
+    const menuItems = menuList.querySelectorAll('li');
+
+    const handlerMenu = () => {
+      menuList.classList.toggle('active-menu');
+    };
+
+    menuBtn.addEventListener('click', handlerMenu);
+    closeBtn.addEventListener('click', handlerMenu);
+
+    menuItems.forEach( item => item.addEventListener('click', handlerMenu) );
+  };
+
+  // PopUp
+  const togglePopUp = () => {
+    const popup         = document.querySelector('.popup');
+    const popupOpenBtn  = document.querySelectorAll('.popup-btn');
+    const popupCloseBtn = document.querySelectorAll('.popup-close');
+
+    popupOpenBtn.forEach((item) =>
+      item.addEventListener('click', () => {
+        popup.style.display = 'block';
+        if (screen.width >= 768) popupOpenAnimation();
+      })
+    );
+
+    popupCloseBtn.forEach((item) =>
+      item.addEventListener('click', () => {
+        if (screen.width >= 768) popupCloseAnimation();
+        else {
+          popup.style.display = 'none';
+        }
+      })
+    );
+  };
+
   const deadline = new Date(2021, 3, 24, 23, 59);
   timer(deadline);
+
+  toggleMenu();
+
+  togglePopUp();
 });
